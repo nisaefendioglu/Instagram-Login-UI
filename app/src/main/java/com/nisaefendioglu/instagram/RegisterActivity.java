eb969d84a632e51a8554a4307a8d83be9a728cb5
nisaefendioglu <nisaefendioglu0@gmail.com>
2021-06-24 14:14:28 +0300

    The reset password page has been designed.

---
+++ app/src/main/java/com/nisaefendioglu/instagram/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.nisaefendioglu.instagram;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class RegisterActivity extends Activity {
+    private String[] countries = {"Afrikaans", "Čeština", "Dansk", "Deutsch", "Ελληνικά","English","English (UK)"
+    , "Español (España)", "Español","Suomi", "Français","Bahasa Indonesia","Italiano", "日本語","한국어","Bahasa Melayu",
+    "Norsk","Nederlands","Polski","Português (Brasil)","Português (Portugal)","Русский","Svenska","ภาษาไทย","Filipino",
+    "Türkçe", "中文(简体)","中文(台灣)",};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+        ListView list=(ListView) findViewById(R.id.country);
+        ArrayAdapter<String> dataAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, countries);
+        list.setAdapter(dataAdapter);
+
+
+    }
+
+}
